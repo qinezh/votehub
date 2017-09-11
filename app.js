65edf2b686f1a47e734ba8c7e3f139c51ec538e9
Qinen Zhu <qinezh@users.noreply.github.com>
2017-09-11 20:28:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(express.static(path.join(root, 'dist')));
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Credentials', "true");
     res.setHeader("Access-Control-Allow-Origin", "https://qinezh.github.io");
+    res.setHeader("Access-Control-Allow-Origins", "https://qinezh.github.io");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, authorization');
     next();
